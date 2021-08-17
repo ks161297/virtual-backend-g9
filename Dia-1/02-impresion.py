964babeaabb63ffc1ee424a7f344c5ce60954822
ks161297 <mksss161297@gmail.com>
2021-08-16 21:48:54 -0500

    feat:agregue impresion

---
+++ Dia-1/02-impresion.py
@@ -0,0 +1,13 @@
+mes = "Agosto"
+
+print("El mes es:", mes)
+
+dia = 16
+
+print("el dia:", dia, "del mes:", mes)
+
+alumnos = 23
+grupo = 9
+
+print("los alumnoa son {1} y son del grupo {0} y todos están atentos". format(alumnos, grupo))
+print(f"los alumnos son {alumnos}")
