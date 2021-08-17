036aacbba9d5690125b1adc9ea63d44f02c23569
ks161297 <mksss161297@gmail.com>
2021-08-16 21:22:50 -0500

    feat:agregue el archivo variables.py

---
+++ Dia-1/01-variables.py
@@ -0,0 +1,19 @@
+# Esto es un comentario :)
+
+# Variables de texto
+
+nombre = "Marigrace"
+apellido = 'Silva'
+
+direccion = "calle o higgins 125"
+
+texto = """Hellooooo, Buenas noches, gud afternoon"""
+
+texto2 = '''Helloooooooo, hello, ya termino'''
+
+#Variables numéricas 
+
+año = 2021
+estatura = 1.55
+
+print(type(nombre))
