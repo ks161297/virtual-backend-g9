b3c84c5049cd6841a45d874a3994ccc84b544002
ks161297 <mksss161297@gmail.com>
2021-08-17 20:36:08 -0500

    chore: seguimiento de Dia2s

---
+++ Dia-2/05-Operadores-Logicos.py
@@ -0,0 +1,17 @@
+# y => significa que ambas comparaciones tienen que ser verdaderas para que todo sea verdadero
+# En Js es && 
+# en python es 'and'
+print((10 > 5) and (100 < 500))
+
+# o => significa que basta con que alguna de las comparaciones sea verdadera para que todo sea verdadero
+#En JS es || (alt + 124)
+#En python es or
+
+print((10 < 5) or (50 > 1))
+
+#        TABLA DE LA VERDAD
+#   V1      V2      /       AND     /       OR
+#   V       V               V               V
+#   V       F               F               V
+#   F       V               F               V   
+#   F       F               F               F
