597f0db9743bc79d51402a0ae52433cbbbc6b8f9
Marigrace Silva :) <50328933+ks161297@users.noreply.github.com>
2021-10-12 19:50:26 -0500

    chore:final-final

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,4 +1,19 @@
 import {Sequelize} from 'sequelize';
+require("dotenv").config();
 
-export const conexion = new Sequelize("postgresql://postgres:root@localhost:5432/tareas",
-                                        {logging: false});
+export const conexion = new Sequelize(
+    process.env.DATABASE_URL,
+    {
+      logging: false,
+      // para poder usar bd remotas de postgres que requiran configuracion especial
+      dialectOptions:
+        process.env.NODE_ENV === "production" // variable que define heroku en sus servidores indicando el contenido de production
+          ? {
+              // sirve para indicar que la bd no sea necesario que tenga certificado SSL y si es que no lo tiene con el rejectUnauthorized = false  indicaremos que no emita el error y que normal se conecte.
+              ssl: {
+                rejectUnauthorized: false,
+              },
+            }
+          : {},
+    } // logging => indica si que quiere mostrar o no las consultas a la bd
+  );
--- src/config/server.js
+++ src/config/server.js
@@ -4,6 +4,7 @@ import { conexion } from './sequelize';
 import swagger from 'swagger-ui-express';
 import documentacion from '../../swagger.json';
 import cors from 'cors'
+require("dotenv").config();
 
 
 
@@ -24,6 +25,14 @@ export class Server{
                 message: "Bienvenido a mi API"
             });
         });
+        if (process.env.NODE_ENV === "production"){
+            documentacion.host = "tareas-node.herokuapp.com";
+            documentacion.schemes=["https"];
+        }else{
+            documentacion.host = `127.0.0.1:${this.puerto}`;
+            documentacion,schemes = ["http"];
+                
+        }
         this.app.use('/docs', swagger.serve, swagger.setup(documentacion));
         this.app.use(tareasRouter)
     }
