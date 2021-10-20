a05a2f2ef08e0d701223861455631e734c30e7d4
Marigrace Silva :) <50328933+ks161297@users.noreply.github.com>
2021-10-20 15:36:23 -0500

    final final final

--- src/config/server.ts
+++ src/config/server.ts
@@ -18,7 +18,7 @@ export class Server{
     private readonly puerto: unknown;
     constructor(){
         this.app = express();
-        this.puerto = process.env.PORT || 8000;
+        this.puerto = process.env.PORT;
         this.app.use(cors());
         this.bodyParser();
         this.rutas();
